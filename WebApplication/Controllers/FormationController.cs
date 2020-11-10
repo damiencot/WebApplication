be965a4d66e14301d025028714eb321a993ad13f
damiencot <damien.cot@gmail.com>
2020-11-10 14:32:55 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication/Controllers/FormationController.cs
@@ -0,0 +1,45 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class FormationController : Controller
+    {
+        // GET: Formation
+        public ActionResult ToutesLesFormations()
+        {
+            List<Formation> listFormations = new List<Formation>();
+            using (var context = new WebApEntities())
+            {
+                listFormations = context.Formation.ToList();
+            }
+            return View(listFormations);
+        }
+
+        public ActionResult DetailFormation(string nomSeo)
+        {
+            var vm = new FormationAvisViewModel();
+            using (var context = new WebApEntities())
+            {
+                var formationEntity = context.Formation.Where(f => f.NomSeo == nomSeo).FirstOrDefault();
+                if(formationEntity == null)
+                {
+                    return RedirectToAction("Accueil", "Home");
+                }
+                vm.FormationNom = formationEntity.Nom;
+                vm.FormationDescription = formationEntity.Description;
+                vm.FormationNomSeo = nomSeo;
+                vm.FormationUrl = formationEntity.Url;
+                vm.Note = formationEntity.Avis;
+
+            }
+
+            return View();
+        }
+    }
+}
